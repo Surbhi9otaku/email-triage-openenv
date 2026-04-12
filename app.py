3adc0ac84bbffe6a9569394f1851ec8dfa9084a3
Surbhi Kumari <rpsurbhi955@gmail.com>
2026-04-12 15:18:54 +0530

    commit changes

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ import gradio as gr
 def greet():
     return "App running successfully 🚀"
 
-iface = gr.Interface(
-    fn=greet,
-    inputs=[],
-    outputs="text"
-)
+with gr.Blocks() as demo:
+    btn = gr.Button("Run")
+    output = gr.Textbox()
+    btn.click(fn=greet, inputs=None, outputs=output)
 
-iface.launch()
+demo.launch(server_name="0.0.0.0", server_port=7860)
